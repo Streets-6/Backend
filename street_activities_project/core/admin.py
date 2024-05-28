61cb56d255e503d8debc2db852f306ff6fc84730
Vladislav Cherepanov <V.V.Cherepanov@bk.ru>
2024-05-28 10:41:51 +0500

    create infrastuctures models

--- street_activities_project/core/admin.py
+++ street_activities_project/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Discipline
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title")
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Discipline, DisciplineAdmin)
